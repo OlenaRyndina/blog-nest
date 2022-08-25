1c57ce69808ada5411999f64b806a8df1e91ee82
Olena Ryndina <olenaryndina1111@gmail.com>
2022-08-25 16:10:36 +0300

    add mysql, add migration

---
+++ ormconfig.ts
@@ -0,0 +1,21 @@
+import { DataSourceOptions } from 'typeorm';
+
+const config: DataSourceOptions = {
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "",
+  database: "blog",
+  entities: ["dist/**/*.entity{.ts,.js}"],
+  dropSchema: false,
+  synchronize: true,
+  migrationsRun: false,
+  logging: true,
+  migrations: ["dist/src/**/db/migrations/*{.ts,.js}"],
+  /*"cli": {
+    "migrationsDir": "db/migrations"
+  }*/
+};
+
+export default config;
